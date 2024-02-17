099f1c6994bf06e1faf2f4d554d73662da8bb6b2
DanTheBeginner09 <148887741+DanTheBeginner09@users.noreply.github.com>
2024-02-18 00:21:27 +0800

    branches with linear color coding

---
+++ app/src/main/java/com/example/villanuevadanielapp/linearColorCoding.java
@@ -0,0 +1,14 @@
+package com.example.villanuevadanielapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class linearColorCoding extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_color_coding);
+    }
+}
